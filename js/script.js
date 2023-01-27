f6f27852314b160998557d1ba0554b78fc53617a
Ella-02-07 <116834315+Ella-02-07@users.noreply.github.com>
2023-01-27 12:10:01 +0200

    add change

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// const totalPrice = 20.7
+// console.log('Price', totalPrice);
+
+// const shouldRenew = confirm('You want continue');
+// console.log(shouldRenew);
+
+// let quantity = prompt('enter quantity goods');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = '50px';
+// elementWidth = Number.parseInt(elementWidth);
+// console.log('elementWidth:', elementWidth);
+
+
+// let salary = 1300.445098;
+// salary = Number(salary.toFixed(2));
+// // salary = Number(salary);
+// console.log(salary);
+
+// const base = 2;
+// const power = 5;
+// const result = base ** power;
+// const result = Math.pow(base, power);
+// console.log(result);
+// console.log(2 ** 5); ** оператор экспонента вместо обЪект Math с функцией pow Math.pow
+
+  
+ 
